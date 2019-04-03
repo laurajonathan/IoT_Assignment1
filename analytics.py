e3a98a1314665bea74ba9f2186aa1bd894015c49
Laura Jonathan <laurajonathan@davins-air-2.home>
2019-04-04 00:09:06 +1100

    Working on analytics.py

--- analytics.py
+++ analytics.py
@@ -6,19 +6,55 @@ Created by Suwat Tangtragoonviwatt (s3710374) and Laura Jonathan (s3696013)
 This script is show data visualization
 
 """
-
 import matplotlib.pyplot as plt
-import csv
 
-with open('report.csv','r') as csvfile:
-    plots = csv.reader(csvfile, delimiter=',')
+from monitorAndNotify import Data
+from monitorAndNotify import Database
+
+class Analytics:
+    """
+    Analytics class for visualize data obtained from the sensehat
+    """
+
+    def __init__(self):
+        pass
+    
+    def generate_linegraph(self, temp, humid, date):
+        """
+        Generate linegraph of temperature and humidity
+        """
+        plt.plot(date, temp, color='g')
+        plt.plot(date, humid, color='orange')
+        plt.xlabel('Date')
+        plt.ylabel('Range')
+        plt.title('Line Graph')
+        plt.legend(['Green = Temperature', 'Orange = Humidity'], loc='upper left')
+        plt.show()
+
+def main():
+        """
+        Main Method
+        """
+
+        # Initialization
+        data = Data()
+        database = Database()
+
+        # Get the data from the database
+        #obtained_data = database.read_data()
+
+        # Extract temperature, humidity and timestamp from database
+        obtained_data = []
+        
+        for temp, humid, timestamp in database.read_data():
+            date = timestamp.strftime('%d/%m/%Y')
+            formatted_data = [[temp], [humid], [date]]
+            obtained_data.append(formatted_data)
+        print(obtained_data)
+        
+        #analytics = Analytics()
+        #analytics = analytics.generate_linegraph()        
 
 
-
-sizes = [70, 30]
-labels = ["OK", "BAD"]
-
-plt.pie(sizes, labels = labels, autopct = "%.2f")
-plt.axes().set_aspect("equal")
-plt.title("Proportion of OK status and bad status")
-plt.show()
+if __name__ == "__main__":
+    main()
